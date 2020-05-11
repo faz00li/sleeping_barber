cb5c2fd944b4d6f0497bc94f5dcb1ca4f9c06a2f
Eddie Raskin <eddie.raskin@gmail.com>
2020-05-11 09:44:38 -0700

    Initial Commit

---
+++ current_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  current_project
+//
+//  Created by faz00li on 5/11/20.
+//  Copyright © 2020 faz00li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
